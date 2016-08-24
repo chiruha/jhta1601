f8147c109d9e90ca4042182b67cb6319bcabb29d
JHTA-PC\ko <arbileros@hanmail.net>
2016-08-24 14:58:57 +0900

    추가

--- src/main/java/fitness/controller/PtregisterController.java
+++ src/main/java/fitness/controller/PtregisterController.java
@@ -1,8 +1,37 @@
 package fitness.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fitness.service.PtregisterService;
+
+
 
 @Controller
 public class PtregisterController {
+	@Autowired private PtregisterService service;
 	
+	@RequestMapping(value="/ptrinsert",method=RequestMethod.GET)
+	public String insert(){
+		return "PtInsert";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

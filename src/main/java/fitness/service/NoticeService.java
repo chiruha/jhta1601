8db195aacd1000c48ed4ed45c1e5091f33f07a18
kimdo87 <diseke@naver.com>
2016-08-29 16:39:41 +0900

    notice 공지사항

---
+++ src/main/java/fitness/service/NoticeService.java
@@ -0,0 +1,37 @@
+package fitness.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fitness.dao.NoticeDao;
+import fitness.dto.NoticeDto;
+
+@Service
+public class NoticeService {
+	@Autowired NoticeDao noticeDao;
+	
+	public int insert(NoticeDto dto){
+		return noticeDao.insert(dto);
+	}
+	public int getCount(){
+		return noticeDao.getCount();
+	}
+	public int delete(int num){
+		return noticeDao.delete(num);
+	}
+	public NoticeDto detail(int num){
+		return noticeDao.detail(num);
+	}
+	public List<NoticeDto> list(HashMap<String, Integer>map){
+		return noticeDao.list(map);
+	}
+	public int update(NoticeDto dto){
+		return noticeDao.update(dto);
+	}
+	public List<NoticeDto> list1(HashMap<String, String> map) {
+		return noticeDao.list1(map);
+	}
+}

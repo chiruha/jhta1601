b1dc0ad566cb4b2c21ff4233b40a6836063dbfef
chiruha <chiruha@naver.com>
2016-09-02 14:24:45 +0900

    css 수정

--- src/main/java/com/project/fitness/MainController.java
+++ src/main/java/com/project/fitness/MainController.java
@@ -1,17 +1,21 @@
 package com.project.fitness;
 
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 	
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home( Model model) {
-	
+	public String home(@RequestParam(value="test", defaultValue="none") String test) {
+		if(test.equals("goboot")){
+			System.out.println("∏ﬁ¿Œ : "+test);
+			return ".bootmain";
+		}
 		return ".main";
 	}
 	
